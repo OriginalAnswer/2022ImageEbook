46e0692f664bb045b1797d8ee353c71f988f489e
OriginalAnswer <akiseonair@gmail.com>
2022-01-03 22:53:32 +0900

    up

--- self.js
+++ self.js
@@ -1,4 +1,25 @@
-const pgNum = document.querySelector('#page-number');
-function savePage {
-    
+const formPage = document.querySelector("#page-number__form");
+const inputPage = document.querySelector("#page-number");
+const pages = document.querySelector(".pages");
+
+const PAGE_KEY = "page";
+const savedPage = localStorage.getItem(PAGE_KEY);
+
+
+function savePage() {
+    const pageNum = inputPage.value;
+    localStorage.setItem(PAGE_KEY, pageNum);
+}
+function loadPage(p) {
+    pages.appendChild(div);
+
+
+}
+
+// console.log(savedPage);
+
+if(savedPage === null){
+    formPage.addEventListener("submit", savePage);
+} else {
+    loadPage(savedPage);
 }
